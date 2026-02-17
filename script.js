f133e2d92335e632128a73127582ba61b23230fb
fcarrascomarin <fcarrascomarin95@gmail.com>
2026-02-16 21:06:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,13 @@ function enviarWhatsApp() {
 
     window.open(url, "_blank");
 }
+
+<script>
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+</script>
+
